9dc3e076120dd491b7c3d0373d683bb945102b5b
Sidor <naxuro@mail.ru>
2021-02-28 20:35:53 +0500

    textures and bug fixes

--- snake.py
+++ snake.py
@@ -4,13 +4,13 @@ import pygame
 import random
 
 # предигровые настройки, фпс и название уровня
-FPS = int(input('Введите FPS (скорость змейки, 2 - средняя): '))
+FPS = int(input('Введите FPS (скорость змейки, 3 - средняя): '))
 filename = 'data/' + input('Введите название уровня (1-5): ') + '.txt'
 pygame.display.set_caption('Перемещение героя')
 with open(filename, 'r') as mapFile:
     level_map = [line.strip() for line in mapFile]
     max_width = max(map(len, level_map))
-    WIDTH, HEIGHT = max_width * 25, len(level_map) * 25
+    WIDTH, HEIGHT = max_width * 50, len(level_map) * 50
 
 
 def close():  # выход из игры
@@ -57,9 +57,20 @@ player_group = pygame.sprite.Group()
 apple_image = load_image('apple.png')
 border_image = load_image('wall.png')
 grass_image = load_image('grass.png')
-head_image = load_image('head.png')
+head_image_right = load_image('head_right.png')
+head_image_left = load_image('head_left.png')
+head_image_up = load_image('head_up.png')
+head_image_down = load_image('head_down.png')
+body_image_right = load_image('body_right.png')
+body_image_left = load_image('body_left.png')
+body_image_up = load_image('body_up.png')
+body_image_down = load_image('body_down.png')
+tail_image_right = load_image('tail_right.png')
+tail_image_left = load_image('tail_left.png')
+tail_image_up = load_image('tail_up.png')
+tail_image_down = load_image('tail_down.png')
 player_image = load_image('body.png')
-tile_width = tile_height = 25
+tile_width = tile_height = 50
 
 
 class Border(pygame.sprite.Sprite):  # блоки, стены, недвижимые объекты
@@ -94,8 +105,9 @@ class Head(pygame.sprite.Sprite):  # голова змеи
     def __init__(self, snake):
         super().__init__(snake_group, all_sprites)
         self.snake = snake
-        self.image = head_image
         self.head = snake[0]
+        self.tail = snake[1]
+        self.image = self.head_photo(self.head[2])
         self.rect = self.image.get_rect().move(
             self.head[0], self.head[1])
         self.direction = 1, 0
@@ -112,13 +124,13 @@ class Head(pygame.sprite.Sprite):  # голова змеи
 
     def directions(self, coords):  # меняет координаты головы в зависимости от направления
         if self.direction[0] == 1:
-            return coords[0] + 25, coords[1]
+            return coords[0] + 50, coords[1]
         elif self.direction[0] == -1:
-            return coords[0] - 25, coords[1]
+            return coords[0] - 50, coords[1]
         elif self.direction[1] == 1:
-            return coords[0], coords[1] - 25
+            return coords[0], coords[1] - 50
         elif self.direction[1] == -1:
-            return coords[0], coords[1] + 25
+            return coords[0], coords[1] + 50
 
     def move(self):  # меняет координаты всех частей змеи (в том числе и головы)
         f = False
@@ -136,7 +148,7 @@ class Head(pygame.sprite.Sprite):  # голова змеи
             copy.append(i)
         for i in range(len(self.snake)):
             if not i:
-                copy[i] = self.directions(self.snake[i])
+                copy[i] = (self.directions(self.snake[i])[0], self.directions(self.snake[i])[1], self.direction)
                 self.head = copy[0]
             else:
                 copy[i] = self.snake[i - 1]
@@ -145,21 +157,56 @@ class Head(pygame.sprite.Sprite):  # голова змеи
         if f:
             copy.append(self.snake[-1])
         self.snake = copy
+        self.tail = self.snake[-1]
+
+    def head_photo(self, direction):
+        if direction == (0, 1):
+            return head_image_up
+        if direction == (0, -1):
+            return head_image_down
+        if direction == (1, 0):
+            return head_image_right
+        if direction == (-1, 0):
+            return head_image_left
 
     def render(self):  # визуальный рендер змеи
-        self.image = head_image
+        self.image = self.head_photo(self.direction)
         self.rect = self.image.get_rect().move(
             self.head[0], self.head[1])
         for i in self.snake:
-            if i != self.head:
-                Body(self.image.get_rect().move(
-                    i[0], i[1]))
+            if i != self.head and i != self.tail:
+                Body(player_image.get_rect().move(
+                    i[0], i[1]), i[2])
+            elif i == self.tail:
+                Tail(player_image.get_rect().move(
+                    i[0], i[1]), i[2])
+
+
+class Tail(pygame.sprite.Sprite):  # кусочки тела змеи
+    def __init__(self, rect, direction):
+        super().__init__(player_group, all_sprites)
+        if direction == (0, 1):
+            self.image = tail_image_up
+        elif direction == (1, 0):
+            self.image = tail_image_right
+        elif direction == (0, -1):
+            self.image = tail_image_down
+        elif direction == (-1, 0):
+            self.image = tail_image_left
+        self.rect = rect
 
 
 class Body(pygame.sprite.Sprite):  # кусочки тела змеи
-    def __init__(self, rect):
+    def __init__(self, rect, direction):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
+        if direction == (0, 1):
+            self.image = body_image_up
+        elif direction == (1, 0):
+            self.image = body_image_right
+        elif direction == (0, -1):
+            self.image = body_image_down
+        elif direction == (-1, 0):
+            self.image = body_image_left
         self.rect = rect
 
 
@@ -181,8 +228,9 @@ if __name__ == '__main__':  # основная программа
     generate_level(load_level(filename))
     pos = random_apple(load_level(filename))
     Apple(pos[0], pos[1])
-    snake = Head([(150, 150), (125, 150), (100, 150)])  # здесь задается начальное кол-во частей тела и их координаты
+    snake = Head([(150, 150, (0, 1)), (125, 150, (0, 1)), (100, 150, (0, 1))])
     running = True
+    action_happened = False
     counter = 20 // FPS  # для стабильной работы поворотов и в целом самой игры я сделал такой подход к ФПС
     all_sprites.draw(screen)  # отрисовка спрайтов для начала игры
     snake_group.draw(screen)
@@ -194,25 +242,30 @@ if __name__ == '__main__':  # основная программа
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and not action_happened:
                 if event.key == pygame.K_LEFT:
                     if prev != 'right':
+                        action_happened = True
                         snake.set_direction('left')
                         prev = 'left'
                 if event.key == pygame.K_RIGHT:
                     if prev != 'left':
+                        action_happened = True
                         snake.set_direction('right')
                         prev = 'right'
                 if event.key == pygame.K_UP:
                     if prev != 'down':
+                        action_happened = True
                         snake.set_direction('up')
                         prev = 'up'
                 if event.key == pygame.K_DOWN:
                     if prev != 'up':
+                        action_happened = True
                         snake.set_direction('down')
                         prev = 'down'
         clock.tick(20)
         if counter == 20 // FPS:  # при каждом значении FPS что-то будет происходить
+            action_happened = False
             counter = 0
             snake.move()
             snake.render()
